dea7b3773b4ededeab1326d7bf409ed8b0ca0eb1
Wyatt Channings <wyattchannings@gmail.com>
2023-09-07 15:20:27 +0800

    Create desktop layout

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 // Navbar Toggle
 function toggleNav() {
-  const navToggle = document.querySelector("#hamburger");
+  const navToggle = document.querySelector("#toggle");
+  const navMenu = document.querySelector("#menu");
   if (navToggle.classList.contains("active")) {
     navToggle.classList.remove("active");
+    navMenu.classList.remove("active");
   } else {
     navToggle.classList.add("active");
+    navMenu.classList.add("active");
   }
 }
 
 function init() {
-  document.querySelector("#hamburger").addEventListener("click", toggleNav);
+  document.querySelector("#toggle").addEventListener("click", toggleNav);
 }
 window.onload = init;
